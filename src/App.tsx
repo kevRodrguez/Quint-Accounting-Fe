2303567aaab2fa7379a2956a4497ea182e7c6a47
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-09-05 22:40:08 -0600

    feat: integrated supabase ui library auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,19 @@
 "use client"
-import * as React from "react"
-import { ChevronDownIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import { Label } from "@/components/ui/label"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+import { Route, Routes } from 'react-router-dom'
 
 import './App.css'
+import Home from './home'
+import { LoginForm } from './components/login-form'
+import { SignUpForm } from './components/sign-up-form'
 
 function App() {
-  const [open, setOpen] = React.useState(false)
-  const [date, setDate] = React.useState<Date | undefined>(undefined)
+
   return (
-    <div className="flex flex-col gap-3">
-      <Label htmlFor="date" className="px-1">
-        Date of birth
-      </Label>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            id="date"
-            className="w-48 justify-between font-normal"
-          >
-            {date ? date.toLocaleDateString() : "Select date"}
-            <ChevronDownIcon />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
-          <Calendar
-            mode="single"
-            selected={date}
-            captionLayout="dropdown"
-            onSelect={(date) => {
-              setDate(date)
-              setOpen(false)
-            }}
-          />
-        </PopoverContent>
-      </Popover>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/sign-up" element={<SignUpForm />} />
+    </Routes>
   )
 }
 
---
+++ src/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { useNavigate } from 'react-router-dom'
+
+export default function home() {
+
+    const navigate = useNavigate();
+    return (
+        <div>
+            este es el home
+
+            <Button onClick={() => navigate('/login')}>Sign in</Button>
+        </div>
+    )
+}
