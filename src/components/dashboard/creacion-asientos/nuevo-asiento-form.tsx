37f0930616be17d2d1cb28f7e5051acaf6d72d1c
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-09-11 23:41:38 -0600

    feat: added basic modal to add new asiento

---
+++ src/components/dashboard/creacion-asientos/nuevo-asiento-form.tsx
@@ -0,0 +1,101 @@
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useContext, useState } from 'react'
+import { AuthContext } from '@/context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import QuintIconRed from '@/assets/quint-icons/quint-icon-red.png';
+import { Calendar } from '@/components/ui/calendar'
+import { DatePicker } from '@/components/ui/date-picker'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { CalendarIcon } from 'lucide-react'
+import { format } from 'date-fns'
+import { date } from 'zod'
+
+export function NuevoAsientoForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const [descripcion, setDescripcion] = useState('');
+    const [fecha, setFecha] = useState<Date | undefined>(new Date());
+
+    const navigate = useNavigate();
+
+    // Si ya hay una sesión activa, redirigir al dashboard
+
+
+
+    return (
+
+
+        <div className={cn('w-full max-w-[500px] min-w-[300px] flex flex-col gap-6 px-4', className)} {...props}>
+            <Card style={{ color: '#fafafa', backgroundColor: '#171717' }} className='m-3'>
+                <CardHeader>
+                    <CardTitle className="text-2xl">Nuevo Asiento</CardTitle>
+                    <CardDescription>Ingresa los detalles del nuevo asiento a continuación</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={() => console.log("enviando")}>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="descripcion">Descripcion</Label>
+                                <Input
+                                    id="descripcion"
+                                    type="text"
+                                    placeholder="Descripción del asiento"
+                                    required
+                                    value={descripcion}
+                                    onChange={(e) => setDescripcion(e.target.value)}
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="fecha">Fecha</Label>
+
+
+                                {/* calendario */}
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Button
+                                            variant="outline"
+                                            data-empty={!fecha}
+                                            style={{ backgroundColor: 'rgb(23 23 23)', color: 'rgb(229 229 229)' }}
+                                            className="data-[empty=true]:text-muted-foreground w-[280px] justify-start text-left font-normal"
+                                        >
+                                            <CalendarIcon />
+                                            {fecha ? format(fecha, "PPP") : <span>Selecciona una fecha</span>}
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-auto p-0">
+                                        <Calendar mode="single" selected={fecha} onSelect={setFecha} />
+                                    </PopoverContent>
+                                </Popover>
+                            </div>
+
+
+                            <Button type="submit" className="w-full" disabled={isLoading} style={{ backgroundColor: '#e5e5e5', color: 'black' }}>
+                                {isLoading ? 'Logging in...' : 'Login'}
+                            </Button>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            No tienes una cuenta?{' '}
+                            <a href="/sign-up" className="underline underline-offset-4">
+                                Registrarse
+                            </a>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+
+    )
+}
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -3,34 +3,83 @@ import { AppSidebar } from '@/components/dashboard/app-sidebar';
 import { ChartAreaInteractive } from '@/components/dashboard/chart-area-interactive';
 import { SectionCards } from '@/components/dashboard/section-cards';
 import { SiteHeader } from '@/components/dashboard/site-header';
+import { Button } from '@/components/ui/button';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import React from 'react'
-
+import Modal from 'react-modal';
+import { LoginForm } from '../auth/login-form';
+import { NuevoAsientoForm } from '@/components/dashboard/creacion-asientos/nuevo-asiento-form';
 export default function Dashboard() {
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
 
+        },
+    };
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        console.log("cerrando modal")
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
     return (
-        <SidebarProvider
-            style={
-                {
-                    "--sidebar-width": "calc(var(--spacing) * 72)",
-                    "--header-height": "calc(var(--spacing) * 12)",
-                } as React.CSSProperties
-            }
+        //Modal para nuevo asiento
+        <><Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={{
+                ...customStyles,
+                content: {
+                    ...customStyles.content,
+                    backgroundColor: 'transparent',
+                    border: 'none', // Cambia aquí el color de fondo
+                },
+            }}
+            contentLabel="Example Modal"
         >
-            <AppSidebar variant="inset" />
-            <SidebarInset>
-                <SiteHeader />
-                <div className="flex flex-1 flex-col">
-                    <div className="@container/main flex flex-1 flex-col gap-2">
-                        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                            <SectionCards />
-                            <div className="px-4 lg:px-6">
-                                <ChartAreaInteractive />
+            <NuevoAsientoForm />
+        </Modal><>
+
+                <SidebarProvider
+                    style={{
+                        "--sidebar-width": "calc(var(--spacing) * 72)",
+                        "--header-height": "calc(var(--spacing) * 12)",
+                    } as React.CSSProperties}
+                >
+                    <AppSidebar variant="inset" />
+                    <SidebarInset>
+                        <SiteHeader />
+
+                        {/* Botón provisional para añadir nuevo asiento */}
+                        <Button style={{ width: "10%", marginLeft: '4%', marginTop: '4%' }} onClick={openModal}>Añadir asiento</Button>
+                        <div className="flex flex-1 flex-col">
+                            <div className="@container/main flex flex-1 flex-col gap-2">
+                                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                                    <SectionCards />
+                                    <div className="px-4 lg:px-6">
+                                        <ChartAreaInteractive />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </SidebarInset>
-        </SidebarProvider>
+                    </SidebarInset>
+                </SidebarProvider></></>
+
+
     )
 }
+
+
