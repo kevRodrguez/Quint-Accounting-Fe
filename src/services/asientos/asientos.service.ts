a437ab3c2bebeb2d503dcae6dc0dc2c7ab4204d5
andrees205 <andresgalan725@gmail.com>
2025-09-28 20:03:40 -0600

    editar asientos

--- src/services/asientos/asientos.service.ts
+++ src/services/asientos/asientos.service.ts
@@ -4,7 +4,12 @@ import api from "@/lib/axios";
 import type { AxiosResponse } from "axios";
 import axios from "axios";
 import { toast } from "react-toastify";
-
+import type {
+  AsientoEdit,
+  AsientosConTotale,
+  DetalleAsiento,
+} from "@/types/libroDiario.interface";
+import type { Movimiento } from "@/types/movimiento.interface";
 
 export class AsientosService {
   public static async crearAsiento(
@@ -27,9 +32,13 @@ export class AsientosService {
     }
   }
 
-  public static async eliminarAsiento(id_asiento: number): Promise<AsientoResponse> {
+  public static async eliminarAsiento(
+    id_asiento: number
+  ): Promise<AsientoResponse> {
     try {
-      const response: AxiosResponse<AsientoResponse> = await api.delete(`libro-diario/asientos/${id_asiento}`);
+      const response: AxiosResponse<AsientoResponse> = await api.delete(
+        `libro-diario/asientos/${id_asiento}`
+      );
       console.log("asiento eliminado " + response.data);
       return response.data;
     } catch (error) {
@@ -41,4 +50,36 @@ export class AsientosService {
       throw new Error("Error desconocido al eliminar asiento");
     }
   }
+
+  public static async actualizarAsiento(
+    id: number,
+    descripcion: string,
+    fecha: Date,
+    movimientos: Movimiento[]
+  ): Promise<AsientoResponse> {
+    try {
+      const response: AxiosResponse<AsientoResponse> = await api.put(
+        `libro-diario/asientos/${id}`,
+        {
+          descripcion,
+          fecha,
+          movimientos,
+        }
+      );
+
+      console.log("Respuesta API:", response.data);
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const message =
+          error.response?.status +
+          " " +
+          (error.response?.data?.message || error.message);
+        throw new Error(`Error al actualizar asiento: ${message}`);
+      }
+      throw new Error("Error desconocido al actualizar asiento");
+    }
+  }
+
+  
 }
